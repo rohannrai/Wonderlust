8f60df14d2ddda78a28ab3de681b6c8fed23856d
rohannrai <rairohan2005@gmail.com>
2024-12-09 18:36:33 +0530

    booking button added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV != "production"){
     require("dotenv").config();
 }
 const express = require("express");
-const app = express();
+const app = express();  
 const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require("./models/listing.js");
