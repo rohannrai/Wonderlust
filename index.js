748d30d7f22061adaf2727b89e0304b20ffbbeea
rohannrai <rairohan2005@gmail.com>
2024-12-13 02:49:47 +0530

    google sheets scripting added

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ const Localstrategy = require("passport-local");
 const multer = require("multer");
 const{storage} = require("./cloudinary.js");
 const upload = multer({  storage })
+const bodyParser = require('body-parser');
+const { google } = require('googleapis');
+
+const KEY_PATH = path.join(__dirname, '/credentials.json');
+const SPREADSHEET_ID = '1BeOQ5wIgOT08r-2eUZ6te8wHyo6f7qhn19cmBUo2rmk';
+const RANGE = 'Sheet1!A1:D1';
 
 main().then(
     () => { console.log("server connected to Db")}
@@ -39,6 +45,7 @@ app.set("views",path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodoverride("_method"));
 app.engine("ejs", ejsMate);
+app.use(bodyParser.json());
 const store = Mongostore.create({
     mongoUrl: MONGO_URL,
     crypto:{
@@ -67,9 +74,11 @@ app.use(express.static(path.join(__dirname , "public")));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: true }));
 passport.use( new Localstrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next) =>{
     res.locals.success = req.flash("success");
@@ -78,6 +87,40 @@ app.use((req,res,next) =>{
     res.locals.curruser = req.user;
     next();
 });
+async function authenticate() {
+    const auth = new google.auth.GoogleAuth({
+      keyFile: KEY_PATH,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'],
+    });
+  
+    const client = await auth.getClient();
+    const sheets = google.sheets({ version: 'v4', auth: client });
+  
+    return sheets;
+  }
+  async function appendToSheet(spreadsheetId, range, values) {
+    try {
+      const sheets = await authenticate();  // Authenticate and get sheets client
+  
+      const resource = {
+        values: [values],  // Values to append as a 2D array
+      };
+  
+      const response = await sheets.spreadsheets.values.append({
+        spreadsheetId,
+        range,
+        valueInputOption: 'RAW',  // You can use 'RAW' or 'USER_ENTERED' depending on your need
+        resource,
+      });
+  
+      return response.data;
+    } catch (error) {
+      console.error('Error appending data to Google Sheets:', error);
+      throw error;
+    }
+  }
+  
+  
 const validatelisting = ( req, res, next) => {
     let {error} = listingschema.validate(req.body);
 if(error){
@@ -288,6 +331,24 @@ app.post("/signup" , wrapasync( async ( req,res) =>{
 app.get("/login" , (req,res) =>{
     res.render("./Users/login.ejs");
 });
+app.post('/submit', async (req, res) => {
+    const { name, hotel , owner , From , To} = req.body;
+  
+    // Google Sheets details
+    const spreadsheetId = '1BeOQ5wIgOT08r-2eUZ6te8wHyo6f7qhn19cmBUo2rmk'; // Replace with your Google Sheet ID
+    const range = 'Sheet1!A:E'; // Specify the range you want to write to (Sheet1, columns A and B)
+  
+    try {
+      // Append the form data (name, email) to Google Sheets
+      const response = await appendToSheet(spreadsheetId, range, [ name, hotel , owner , From , To]);
+      console.log('Data successfully added:', response);
+      res.redirect("/listings")
+    } catch (error) {
+      res.status(500).send('Error submitting the form.');
+      console.log(error)
+    }
+   
+  });
 
 app.post("/login" ,saveredirecturl,  passport.authenticate( "local" , { 
     failureRedirect:"/login", 
